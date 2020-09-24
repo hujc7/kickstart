c86664d4a0bd9cc4d683a88fc5e171904f32f2c2
Jichuan Hu <leo.hu.sh@gmail.com>
2020-09-24 03:06:14 -0400

    Passed 2020C D!
    One time pass for D.

--- 2020C/D.cpp
+++ 2020C/D.cpp
@@ -56,8 +56,84 @@ template<class T> void print(vector<T>& v) {
     write("\n");
 }
 
+int lowbit(int x) {
+    return x & (-x);
+}
+
+class BIT {
+public:
+    int m;
+    vector<ll> v;
+    vector<ll> bit;
+    BIT(int m) {
+        this->m = m;
+        v = vector<ll>(m);
+        bit = vector<ll>(m+1);
+    }
+
+    void update(int index, ll u) {
+        ll diff = u - v[index];
+        v[index] = u;
+
+        for (int i = index+1; i <= m; i += lowbit(i)) {
+            bit[i] += diff;
+        }
+    }
+
+    ll query(int index) {
+        ll res = 0;
+        for (int i = index+1; i > 0; i -= lowbit(i)) {
+            res += bit[i];
+        }
+
+        return res;
+    }
+
+    int lowbit(int x) {
+        return x & (-x);
+    }
+};
+
+ll getSign(int i) {
+    return i % 2 == 0 ? 1 : -1;
+}
+
 void solve() {
-    
+    int N, Q; cin >> N >> Q;
+    vector<ll> v(N);
+    read(v);
+
+    BIT bit1(N), bit2(N);
+
+    for (int i = 0; i < N; ++i) {    
+        ll c = v[i];
+        ll sign = getSign(i);
+        bit1.update(i, sign * (i+1) * c);
+        bit2.update(i, sign * c);
+    }
+
+    ll res = 0;
+    while (Q--) {
+        char c;
+        ll l, r;
+        cin >> c >> l >> r;
+        l--;
+        
+        if (c == 'U') {
+            ll sign = getSign(l);
+            bit1.update(l, sign * (l+1) * r);
+            bit2.update(l, sign * r);
+        } else {
+            r--;
+
+            ll sign = getSign(l);
+            ll sum1 = bit1.query(r) - bit1.query(l-1);
+            ll sum2 = bit2.query(r) - bit2.query(l-1);
+            ll tmp = sign * (sum1 - l*sum2);
+            res += tmp;
+        }
+    }
+    cout << res;
 }
 
 int main() {
@@ -65,8 +141,6 @@ int main() {
 
     int t; cin >> t;
     for (int cn = 1; cn <= t; ++cn) {
-        // read input
-
         // solve
         cout << "Case #" << cn << ": ";
         solve();
