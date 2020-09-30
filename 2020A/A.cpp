e29fca60867007c2094083f3055217f7e87eceef
Jichuan Hu <leo.hu.sh@gmail.com>
2020-09-30 13:44:41 -0400

    Passed 2020A a Added templated folder

---
+++ 2020A/A.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+// #include <climits>
+// #include <iostream>
+// #include <vector>
+// #include <utility>
+using namespace std;
+#define vt vector
+#define pb push_back
+#define all(c) (c).begin(), (c).end()
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define fi first
+#define se second
+#define rep(i,L,R) for (int i = L; i < R; ++i)
+#define rrep(i,L,R) for (int i = R; i > L; --i)
+#define each(x, a) for (auto& x: a)
+
+using ul = unsigned long;
+using ll = long long;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vvi = vector<vi>;
+using ii = pair<int, int>;
+
+template<class T> void read(T& x) {
+    cin >> x;
+}
+template <class H, class... T>
+void read(H& h, T&... t) {
+    cin >> h;
+    read(t...);
+}
+template<class T> void read(vector<T>& v) {
+    each(x, v)
+        read(x);
+}
+template<class A> void write(A x) {
+	cout << x;
+}
+template<class H, class... T> void write(const H& h, const T&... t) { 
+	write(h);
+	write(t...);
+}
+void print() {
+	write("\n");
+}
+template<class H, class... T> void print(const H& h, const T&... t) { 
+	write(h);
+	if(sizeof...(t))
+		write(' ');
+	print(t...);
+}
+template<class T> void print(vector<T>& v) {
+    each(x, v)
+        write(x, " ");
+    write("\n");
+}
+
+void solve() {
+    int N, B; cin >> N >> B;
+    vector<int> v(N);
+    read(v);
+
+    sort(v.begin(), v.end());
+
+    int count = 0;
+    for (auto c: v) {
+        if ((B -= c) >= 0) {
+            count++;
+        } else {
+            break;
+        }
+    }
+
+    cout << count;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+    int t; cin >> t;
+    for (int cn = 1; cn <= t; ++cn) {
+        // solve
+        cout << "Case #" << cn << ": ";
+        solve();
+        cout << "\n";
+    }
+
+    return 0;
+}
